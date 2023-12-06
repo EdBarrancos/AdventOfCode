796394a3d9ba6a48bc456d20d946a5d4c39d3aa6
Eduardo Barrancos <eduardobarrancos11488@gmail.com>
2023-12-06 09:02:19 +0000

    Exercise 6

---
+++ 2023/AdventOfCode2023/src/Exercise6.jl
@@ -0,0 +1,53 @@
+#= 
+
+for each milisecond holding -> 1 more milimeter per second
+
+Nunca desceleras
+
+=#
+
+
+input = "Time:        52     94     75     94
+Distance:   426   1374   1279   1216"
+
+struct Race
+    time
+    distance
+end
+
+function get_list_of_numbers_in_line(line)
+    numbers_only = split(line, ": ")[end]
+    a =  filter(n -> !isspace(n), numbers_only)
+    println(a)
+    return a
+end
+
+map_numbers_to_int(array_of_numbers) = map(number -> parse(Int, number), array_of_numbers)
+
+function parse_input(input)
+    lines = split(input, '\n')
+    time = parse(Int, get_list_of_numbers_in_line(lines[begin]))
+    distance = parse(Int, get_list_of_numbers_in_line(lines[end]))
+    return map(x -> Race(x[end], distance[x[begin]]), enumerate(time))
+end
+
+function is_winnable_with_charging_time(charging_time::Int, race::Race)
+    left_over_time = race.time - charging_time
+    return left_over_time * charging_time > race.distance
+end
+
+function number_of_winnable_configs(race::Race)
+    max_time = race.time
+    winnable_points = filter(point -> is_winnable_with_charging_time(point, race), 0:max_time)
+    return length(winnable_points)
+end
+
+function exercise6(input)
+    races = parse_input(input)
+    println(races)
+    reduce(*, map(number_of_winnable_configs, races))
+end
+
+exercise6(input)
+
+div(15, 2)
