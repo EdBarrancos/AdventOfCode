51fc97c197c80eba7dd1fce3a7604662e73c85dc
Eduardo Barrancos <eduardobarrancos@premium-minds.com>
2024-12-04 09:31:57 +0000

    Day 4 - Part 2

--- 2024/Day4.jl
+++ 2024/Day4.jl
@@ -1,6 +1,7 @@
 struct X end
 struct M end
 struct A end
+struct S end
 
 function found(::X, map, current_pos::Pair{Int,Int}, dir::Pair{Int,Int})::Bool
     search_in = Pair(current_pos[1] + dir[1], current_pos[2] + dir[2])
@@ -12,17 +13,28 @@ function found(::X, map, current_pos::Pair{Int,Int}, dir::Pair{Int,Int})::Bool
     end
     return false
 end
-function found(::M, map, current_pos::Pair{Int,Int}, dir::Pair{Int,Int})::Bool
+function looking_for(::M, map, current_pos::Pair{Int,Int}, dir::Pair{Int,Int})::Bool
+    search_in = Pair(current_pos[1] + dir[1], current_pos[2] + dir[2])
+    if length(map) < search_in[1] || search_in[1] < 1 || search_in[2] < 1 || length(map[1]) < search_in[2]
+        return false
+    end
+    if map[search_in[1]][search_in[2]] == 'M'
+
+        return looking_for(A(), map, search_in, dir)
+    end
+    return false
+end
+function looking_for(::A, map, current_pos::Pair{Int,Int}, dir::Pair{Int,Int})::Bool
     search_in = Pair(current_pos[1] + dir[1], current_pos[2] + dir[2])
     if length(map) < search_in[1] || search_in[1] < 1 || search_in[2] < 1 || length(map[1]) < search_in[2]
         return false
     end
     if map[search_in[1]][search_in[2]] == 'A'
-        return found(A(), map, search_in, dir)
+        return looking_for(S(), map, search_in, dir)
     end
     return false
 end
-function found(::A, map, current_pos::Pair{Int,Int}, dir::Pair{Int,Int})::Bool
+function looking_for(::S, map, current_pos::Pair{Int,Int}, dir::Pair{Int,Int})::Bool
     search_in = Pair(current_pos[1] + dir[1], current_pos[2] + dir[2])
     if length(map) < search_in[1] || search_in[1] < 1 || search_in[2] < 1 || length(map[1]) < search_in[2]
         return false
@@ -49,15 +61,12 @@ function part1(input_string)
     total = 0
     for x in eachindex(map)
         for y in eachindex(map[1])
-            if map[x][y] == 'X'
-                total += found(X(), map, Pair(x, y), Pair(1, 0)) ? 1 : 0
-                total += found(X(), map, Pair(x, y), Pair(0, 1)) ? 1 : 0
-                total += found(X(), map, Pair(x, y), Pair(1, 1)) ? 1 : 0
-                total += found(X(), map, Pair(x, y), Pair(-1, 0)) ? 1 : 0
-                total += found(X(), map, Pair(x, y), Pair(0, -1)) ? 1 : 0
-                total += found(X(), map, Pair(x, y), Pair(-1, -1)) ? 1 : 0
-                total += found(X(), map, Pair(x, y), Pair(1, -1)) ? 1 : 0
-                total += found(X(), map, Pair(x, y), Pair(-1, 1)) ? 1 : 0
+            right = false
+            left = false
+            if map[x][y] == 'A'
+                right = looking_for(M(), map, Pair(x - 2, y + 2), Pair(1, -1)) || looking_for(M(), map, Pair(x + 2, y - 2), Pair(-1, 1))
+                left = looking_for(M(), map, Pair(x - 2, y - 2), Pair(1, 1)) || looking_for(M(), map, Pair(x + 2, y + 2), Pair(-1, -1))
+                total += right && left ? 1 : 0
             end
         end
     end
